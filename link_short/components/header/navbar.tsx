8fb2fe70920cacfbc46a0dcfe983abb8ac6bd055
sandip parida <sandipparida282@gmail.com>
2023-05-07 21:42:17 +0530

    remove sinin()

--- link_short/components/header/navbar.tsx
+++ link_short/components/header/navbar.tsx
@@ -103,7 +103,7 @@ export default function Navbar() {
                 <button
                   type="button"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  }
+                  
                 >
                   Sign In
                 </button>
